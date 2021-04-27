9bff4814b86301bb622513a1119b676c5fbb71ba
Wellington-Black <well1482@gmail.com>
2021-04-27 15:43:52 +0100

    addes index page for activities

--- studio_management_suite/app.py
+++ studio_management_suite/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 
 # from controllers.attendance_controller import attendance_controller
-# from controllers.activity_controller import activity_controller
+from controllers.activity_controller import activities_blueprint
 from controllers.member_controller import members_blueprint
 
 app = Flask(__name__)
 
 # app.register_blueprint(attendances_blueprint)
-# app.register_blueprint(activities_blueprint)
+app.register_blueprint(activities_blueprint)
 app.register_blueprint(members_blueprint)
 
 @app.route('/')
