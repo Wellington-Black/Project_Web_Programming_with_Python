0591cae650b5bfdaf8c76d05f93caeeb71f5d8ca
Wellington-Black <well1482@gmail.com>
2021-04-27 21:40:53 +0100

    Added created new activity route and html files

--- studio_management_suite/controllers/activity_controller.py
+++ studio_management_suite/controllers/activity_controller.py
@@ -9,3 +9,24 @@ activities_blueprint = Blueprint("activities", __name__)
 def activities():
     activities = activity_repository.select_all()
     return render_template("activities/index.html", activities = activities)
+
+#GET
+#NEW
+@activities_blueprint.route("/activities/new", methods = ['GET'])
+def new_activity():
+    activities = activity_repository.select_all()
+    return render_template("activities/new.html", all_activities = activities)
+
+#CREATE
+#POST
+@activities_blueprint.route("/activities", methods = ['POST'])
+def create_activity():
+    name = request.form['name']
+    instructor = request.form['instructor']
+    time = request.form['time']
+    studio = request.form['studio']
+    level = request.form['level']
+    # member = member_repository.select(member_id)
+    activity = Activity(name, instructor, time, studio, level)
+    activity_repository.save(activity)
+    return redirect('/activities')
